1c69bad9526c232e17357bcb71cab9be31320860
anhphan10 <phananhd09@gmail.com>
2024-10-16 10:46:46 +0700

    update

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -33,7 +33,7 @@ module.exports.index = async (req, res) => {
 //[Post]/checkout/order
 module.exports.order = async (req, res) => {
     const cartId = req.cookies.cartId;
-    
+
     const userInfo = req.body;
     const cart = await Cart.findOne({
         _id: cartId
@@ -73,4 +73,25 @@ module.exports.order = async (req, res) => {
 
 
     res.redirect(`/checkout/success/${order.id}`)
+}
+
+//[Get]/checkout/success/:id
+module.exports.success = async (req, res) => {
+    const order = await Order.findOne({
+        _id: req.params.orderId
+    })
+    for (const product of order.products) {
+        const productInfo = await Product.findOne({
+            _id: product.product_id
+        }).select("title thumbnail");
+        product.productInfo = productInfo;
+        product.priceNew = productHelper.priceNewProduct(product);
+        product.totalPrice = product.priceNew * product.quantity;
+    }
+
+    order.totalPrice = order.products.reduce((sum, item) => sum + item.totalPrice, 0);
+    res.render("client/pages/checkout/success", {
+        pageTitle: "Đặt Hàng Thành Công",
+        order: order
+    })
 }
