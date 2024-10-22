6f7c0c21565ac828b04438257ebdca3e24a06a57
anhphan10 <phananhd09@gmail.com>
2024-10-22 17:24:43 +0700

    logout_feature

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -61,3 +61,8 @@ module.exports.loginPost = async(req, res)=>{
     res.cookie("tokenUser",user.tokenUser)
     res.redirect("/");
     }
+//[Get]/user/logout
+module.exports.logout = async(req,res)=>{
+    res.clearCookie("tokenUser");
+    res.redirect("/");
+}
