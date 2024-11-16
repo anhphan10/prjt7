aa9c24a7efe8235280c65e358ed90022c9f9750f
anhphan10 <phananhd09@gmail.com>
2024-11-16 22:25:29 +0700

    saveDataToCache

--- middlewares/client/setting.middlewares.js
+++ middlewares/client/setting.middlewares.js
@@ -1,6 +1,22 @@
-const SettingGeneral = require("../../models/setting-general.model")
-module.exports.settingGeneral = async (req,res,next) =>{
-    const settingGeneral = await SettingGeneral.findOne({});
-    res.locals.settingGeneral = settingGeneral
-    next();
+const NodeCache = require("node-cache");
+const cache = new NodeCache({stdTTL:60}); //cache initialization lasts for 1 minute
+const SettingGeneral = require("../../models/setting-general.model");
+
+module.exports.settingGeneral = async(req, res, next) =>{
+    try {
+        let settingGeneral = cache.get("settingGeneral");
+        if(!settingGenera){
+        //If the data does not exist in memory, perform a query in the data base
+            const settingGeneral = await SettingGeneral.findOne();
+            console.log(settingGeneral.logo);
+            cache.set("settingGeneral" , settingGeneral);
+        }
+        else{
+            console.log("using cached data")
+        }
+        res.locals.settingGeneral = settingGeneral;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
 }
